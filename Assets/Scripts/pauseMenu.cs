5e1a347708738dbc78f3a689a1472315878845d9
bnostrom <bnostrom@mtu.edu>
2020-04-20 12:16:51 -0400

    Pause functionallity added
    might have to remove sliders

---
+++ Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+    private static bool paused = false;
+    public GameObject pauseMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 2) {
+            if (paused) {
+                resume();
+            }
+            else{
+                pause();
+            }
+        }
+    }
+
+    void resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    void pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+}
