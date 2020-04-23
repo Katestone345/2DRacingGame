c9ed8c6e3ba02d4188a7f9161bad1739cdacbc14
bnostrom <bnostrom@mtu.edu>
2020-04-23 12:55:27 -0400

    Displays Score at the end

---
+++ Assets/Scripts/EndScore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScore : MonoBehaviour
+{
+    public Text txt;
+   void Awake() {
+       txt.text = PlayerPrefs.GetString("score").Substring(6);
+   }
+}
--- Assets/Scripts/collision.cs
+++ Assets/Scripts/collision.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class collision : MonoBehaviour
 {
+    public Text timer;
    private void OnTriggerEnter2D( Collider2D other) {
        this.gameObject.SetActive(false);
+       PlayerPrefs.SetString("score",timer.text);
        SceneManager.LoadScene("GameOver");
    }
 }
